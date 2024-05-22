5ab2efb97f13fd044ce2cd4e2b300db3840db5e2
DAbarboza <bardanii7@gmail.com>
2024-05-21 21:14:47 -0300

    Landing Page terminada

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,12 @@ import "./App.css";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Landing from "./components/LandingPage/landigPage";
-import HomePage from "./components/HomePage/homePage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<Landing />} />
-        <Route exact path="/home" element={<HomePage />} />
       </Routes>
     </div>
   );
